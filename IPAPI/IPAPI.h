e69290a059eedba1ccc71a4b9818e4005a526ea9
Aubrey Goodman <aubrey.goodman@gmail.com>
2016-09-28 12:30:28 -0700

    Initial commit using Swift.gitignore from github

---
+++ IPAPI/IPAPI.h
@@ -0,0 +1,19 @@
+//
+//  IPAPI.h
+//  IPAPI
+//
+//  Created by Aubrey Goodman on 9/28/16.
+//  Copyright © 2016 Aubrey Goodman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IPAPI.
+FOUNDATION_EXPORT double IPAPIVersionNumber;
+
+//! Project version string for IPAPI.
+FOUNDATION_EXPORT const unsigned char IPAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPAPI/PublicHeader.h>
+
+
